8122a282453ab7c8c2d88dcd2d63c0c046ad8c84
adal6a <adalgarcia17@gmail.com>
2021-05-06 21:11:42 -0600

    added test for user authentication

---
+++ backend/tests/Feature/AuthenticationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUserCanAuthenticate()
+    {
+        $password = 'test';
+
+        $user = User::factory()->create([
+            'password' => $password
+        ]);
+
+        $response = $this->json('POST', '/api/v1/login', [
+            'email' => $user->email,
+            'password' => $password
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
