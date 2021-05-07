56d0f947113e90eeb441fdfcb1334965d090524f
adal6a <adalgarcia17@gmail.com>
2021-05-06 22:21:27 -0600

    added header menu nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header>
+        <el-menu :router="true" :default-active="activeIndex" mode="horizontal">
+          <el-menu-item index="login" :route="{ name: 'login' }">
+            <template slot="title">
+              <i class="el-icon-user"></i>
+              <span>Login</span>
+            </template>
+          </el-menu-item>
+          <el-menu-item index="users" :route="{ name: 'users' }">
+            <template slot="title">
+              <i class="el-icon-tickets"></i>
+              <span>Users list</span>
+            </template>
+          </el-menu-item>
+          <el-menu-item index="logout">
+            <template slot="title">
+              <i class="el-icon-switch-button"></i>
+              <span>Logout</span>
+            </template>
+          </el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main><router-view/></el-main>
+    </el-container>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: 'login'
+    }
+  },
+  watch: {
+    $route (to) {
+      this.activeIndex = to.path;
+    }
+  },
+  methods: {
+
+  }
+
+}
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
